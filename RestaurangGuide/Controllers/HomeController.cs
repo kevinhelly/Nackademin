e92b077979713e928f4a165ad17efdb602b6d191
kevinhelly <kevinhelly.kh@gmail.com>
2014-08-29 15:41:15 +0200

    RestaurangGuiden

---
+++ RestaurangGuide/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace RestaurangGuide.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Fish = "";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Fish = "Hej";
+            return View("Index");
+        }
+    }
+}
---
+++ RestaurangGuide/Controllers/LatestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurangGuide.Controllers
+{
+    public class LatestController : Controller
+    {
+        //
+        // GET: /Latest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
