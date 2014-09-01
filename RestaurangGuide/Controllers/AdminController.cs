6db6ef5f5cdb05afea5da9761e0ed860eff6f52b
kevinhelly <kevinhelly.kh@gmail.com>
2014-09-01 14:40:50 +0200

    New Changes

---
+++ RestaurangGuide/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurangGuide.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index(string username, string password)
+        {
+            return View();
+        }
+
+    }
+}
